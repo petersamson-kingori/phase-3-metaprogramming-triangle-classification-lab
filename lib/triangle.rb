5dea92f51e7b72722ec37b72eeef9ff740e53ef9
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-03-24 09:58:37 +0300

    Completed lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    validate_triangle
+  end
+
+  def kind
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    raise TriangleError if [a, b, c].any? { |s| s <= 0 } || a + b <= c || b + c <= a || a + c <= b
+  end
+
+  class TriangleError < StandardError
+  end
 end
